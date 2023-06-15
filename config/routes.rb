b756c457e23a0142d0bc44b2f62c834767406ac8
Anastasiia Deriabina <anastasiia.deriabina@gmail.com>
2023-06-15 16:28:18 +0200

    movies'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new"
+  # post "lists", to: "lists#create"
+  # get "lists/:id", to: "lists#show"
+  # get "bookmarks/new", to: "bookmarks#new"
+  # post "bookmarks", to: "bookmarks#create"
+  # delete "bookmarks/:id", to: "bookmarks#destroy"
   # Defines the root path route ("/")
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
   # root "articles#index"
 end
